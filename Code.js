f33fb7a212da43e5de70646d1d2b74bd11407299
benlcollins <ben@benlcollins.com>
2021-04-10 17:52:29 -0400

    add basic mattermark api call

--- Code.js
+++ Code.js
@@ -1,6 +1,26 @@
 /**
+ * Global Variables
+ */
+const API_KEY = '';
+
+
+/**
  * function to retrieve company data from Mattermark
  */
 function mattermarkCompanyDetails(company) {
-  
+	  
+	company = 'facebook'
+
+	// set up the api
+	const base = 'https://api.mattermark.com/';
+	const endpoint = 'companies/'
+	const query = '?key=' + API_KEY + '&company_name=' + company;
+	const url = base + endpoint + query;
+
+	// call the api
+	const response = UrlFetchApp.fetch(url);
+	console.log(response.getResponseCode());
+	console.log(response.getContentText());
+	
+
 }
